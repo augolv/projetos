cb598b75452a99c9ea38969827bc31d85614d673
augustooliveira0 <125893759+augustooliveira0@users.noreply.github.com>
2023-04-10 18:55:05 -0300

    update projeto

---
+++ age-calculator-app-main/script.js
@@ -0,0 +1,8 @@
+let diaNascimento= document.querySelector('#dia');
+let mesNascimento= document.querySelector('#mes');
+let anoNascimento= document.querySelector('#ano');
+let idadeDia= document.querySelector('#resultado-dias');
+let idadeMes= document.querySelector('#resultado-meses');
+let idadeAno= document.querySelector('#resultado-anos');
+let botao= document.querySelector('.botao');
+
