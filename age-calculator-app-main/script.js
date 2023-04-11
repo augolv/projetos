996d416946b545a00c09e6c1738d1cb850183ee7
augustooliveira0 <125893759+augustooliveira0@users.noreply.github.com>
2023-04-11 11:25:57 -0300

    update projeto

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -1,8 +1,22 @@
-let diaNascimento= document.querySelector('#dia');
-let mesNascimento= document.querySelector('#mes');
-let anoNascimento= document.querySelector('#ano');
-let idadeDia= document.querySelector('#resultado-dias');
-let idadeMes= document.querySelector('#resultado-meses');
-let idadeAno= document.querySelector('#resultado-anos');
-let botao= document.querySelector('.botao');
+const diaNascimento= document.querySelector('#dia');
+const mesNascimento= document.querySelector('#mes');
+const anoNascimento= document.querySelector('#ano');
+const resultadoDia= document.querySelector('#resultado-dias');
+const resultadoMes= document.querySelector('#resultado-meses');
+const resultadoAno= document.querySelector('#resultado-anos');
+const data= new Date();
+const diaAtual= data.getDate();
+const mesAtual= data.getMonth() + 1;
+const anoAtual= data.getFullYear();
+const botao= document.querySelector('.botao');
 
+function calcularIdade(){
+    let idade = anoAtual - Number(anoNascimento.value);
+    console.log(idade);
+    if(mesAtual < mesNascimento || mes === mesNascimento && dia < diaNascimento){
+        idade--;
+        console.log(anoNascimento);
+    }
+}
+
+botao.addEventListener("click", calcularIdade);
