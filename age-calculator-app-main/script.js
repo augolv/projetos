b7e4b0ed5aae0635b01028239be110a3bc7d7e8b
augustooliveira0 <125893759+augustooliveira0@users.noreply.github.com>
2023-04-11 16:04:26 -0300

    update projeto

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -1,21 +1,29 @@
-const diaNascimento= document.querySelector('#dia');
-const mesNascimento= document.querySelector('#mes');
-const anoNascimento= document.querySelector('#ano');
+let inputDia= document.querySelector('#dia');
+let inputMes= document.querySelector('#mes');
+let inputAno= document.querySelector('#ano');
+
+document.addEventListener('input', (event) => {
+    console.log(event.target.valueAsNumber);
+    inputDia= event.target.valueAsNumber;
+})
+
 const resultadoDia= document.querySelector('#resultado-dias');
 const resultadoMes= document.querySelector('#resultado-meses');
 const resultadoAno= document.querySelector('#resultado-anos');
-const data= new Date();
-const diaAtual= data.getDate();
-const mesAtual= data.getMonth() + 1;
-const anoAtual= data.getFullYear();
+const diaAtual= new Date().getDate();
+const mesAtual= new Date().getMonth() + 1;
+const anoAtual= new Date().getFullYear();
 const botao= document.querySelector('.botao');
+const dataNascimento= new Date(`${Number(inputAno.value)}-${Number(inputMes.value)}-${Number(inputDia.value)} `);
 
 function calcularIdade(){
-    let idade = anoAtual - Number(anoNascimento.value);
-    console.log(idade);
-    if(mesAtual < mesNascimento || mes === mesNascimento && dia < diaNascimento){
-        idade--;
-        console.log(anoNascimento);
+    
+    let idade = anoAtual - Number(inputAno.value);
+
+    if(mesAtual < Number(inputMes.value) || mesAtual === Number(inputMes.value) && diaAtual < Number(inputDia.value)){
+        resultadoAno.textContent= `${idade - 1}`
+        resultadoMes.textContent=   12
+        resultadoDia.textContent= diaAtual
     }
 }
 
