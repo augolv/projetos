b187a150d3c571ce315e1656c4587795e65b4055
augustooliveira0 <125893759+augustooliveira0@users.noreply.github.com>
2023-05-15 17:56:46 -0300

    update projeto

--- Bikcraft/js/script.js
+++ Bikcraft/js/script.js
@@ -1,1 +1,26 @@
-console.log("teste");
+// Ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link){
+    const url = location.href;
+    const href= link.href
+
+    if(url.includes(href)){
+        link.classList.add("ativo")
+    }
+}
+
+links.forEach(ativarLink)
+
+// Ativar items do orçamento
+const parametros= new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+    console.log(parametro);
+    const elemento = document.getElementById(parametro)
+    if(elemento){
+        elemento.checked=true;
+    }
+}
+
+parametros.forEach(ativarProduto);
