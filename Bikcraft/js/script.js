b3f0dd4471dbb90bed92f4d5b60afd506ce74f22
augustooliveira0 <125893759+augustooliveira0@users.noreply.github.com>
2023-05-16 16:20:04 -0300

    update projeto

--- Bikcraft/js/script.js
+++ Bikcraft/js/script.js
@@ -45,3 +45,20 @@ function eventosPerguntas(pergunta){
 
 pergunutas.forEach(eventosPerguntas);
 
+// Galeria das bicicletas
+const galeria= document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer= document.querySelector('.bicicleta-imagens')
+
+function trocarImagem(event){
+    const img= event.currentTarget;
+    const media= matchMedia('(min-width:1000px)').matches;
+    if(media){
+    galeriaContainer.prepend(img)
+    }
+}
+
+function eventosGaleria(img){
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
